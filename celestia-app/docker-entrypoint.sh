5f8b3cc72fe8b4c28e6b8fb24fd108a685cfcada
concentricspheres <107502292+concentricspheres@users.noreply.github.com>
2024-12-11 11:17:41 +0000

    removed cosmovisor, celestia binary handles upgrades

---
+++ celestia-app/docker-entrypoint.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ ! -f /cosmos/.initialized ]]; then
+  echo "Initializing!"
+
+  echo "Running init..."
+  celestia-appd init $MONIKER --chain-id $NETWORK --home /cosmos --overwrite
+
+  echo "Downloading config..."
+  SEEDS=$(curl -sL https://raw.githubusercontent.com/celestiaorg/networks/master/$NETWORK/seeds.txt | tr '\n' ',')
+
+  celestia-appd download-genesis $NETWORK --home /cosmos
+  dasel put -f /cosmos/config/config.toml -v $SEEDS p2p.seeds
+  dasel put -f /cosmos/config/config.toml -v "kv" indexer
+
+  if [ -n "$SNAPSHOT" ]; then
+    echo "Downloading snapshot..."
+    curl -o - -L $SNAPSHOT | lz4 -c -d - | tar --exclude='data/priv_validator_state.json' -x -C /cosmos
+  else
+    echo "No snapshot URL defined."
+  fi
+
+  # Check whether we should rapid sync
+  if [ -n "${RAPID_SYNC_URL}" ]; then
+    echo "Configuring rapid state sync"
+    # Get the latest height
+    LATEST=$(curl -s "${RAPID_SYNC_URL}/block" | jq -r '.result.block.header.height')
+    echo "LATEST=$LATEST"
+
+    # Calculate the snapshot height
+    SNAPSHOT_HEIGHT=$((LATEST - 2000));
+    echo "SNAPSHOT_HEIGHT=$SNAPSHOT_HEIGHT"
+
+    # Get the snapshot hash
+    SNAPSHOT_HASH=$(curl -s $RAPID_SYNC_URL/block\?height\=$SNAPSHOT_HEIGHT | jq -r '.result.block_id.hash')
+    echo "SNAPSHOT_HASH=$SNAPSHOT_HASH"
+
+    dasel put -f /cosmos/config/config.toml -v true statesync.enable
+    dasel put -f /cosmos/config/config.toml -v "${RAPID_SYNC_URL},${RAPID_SYNC_URL}" statesync.rpc_servers
+    dasel put -f /cosmos/config/config.toml -v $SNAPSHOT_HEIGHT statesync.trust_height
+    dasel put -f /cosmos/config/config.toml -v $SNAPSHOT_HASH statesync.trust_hash
+  else 
+    echo "No rapid sync url defined."
+  fi
+
+  touch /cosmos/.initialized
+else
+  echo "Already initialized!"
+fi
+
+echo "Updating config..."
+
+# Get public IP address.
+__public_ip=$(curl -s ifconfig.me/ip)
+echo "Public ip: ${__public_ip}"
+
+# Always update public IP address, moniker and ports.
+dasel put -f /cosmos/config/config.toml -v "${__public_ip}:${CL_P2P_PORT}" p2p.external_address
+dasel put -f /cosmos/config/config.toml -v "tcp://0.0.0.0:${CL_P2P_PORT}" p2p.laddr
+dasel put -f /cosmos/config/config.toml -v 10485760 p2p.recv_rate
+dasel put -f /cosmos/config/config.toml -v 10485760 p2p.send_rate
+dasel put -f /cosmos/config/config.toml -v "tcp://0.0.0.0:${CL_RPC_PORT}" rpc.laddr
+dasel put -f /cosmos/config/config.toml -v ${MONIKER} moniker
+dasel put -f /cosmos/config/config.toml -v true prometheus
+dasel put -f /cosmos/config/config.toml -v ${LOG_LEVEL} log_level
+dasel put -f /cosmos/config/config.toml -v 12 mempool.ttl-num-blocks
+dasel put -f /cosmos/config/app.toml -v "0.0.0.0:${RPC_PORT}" json-rpc.address
+dasel put -f /cosmos/config/app.toml -v "0.0.0.0:${WS_PORT}" json-rpc.ws-address
+dasel put -f /cosmos/config/app.toml -v "0.0.0.0:${CL_GRPC_PORT}" grpc.address
+dasel put -f /cosmos/config/app.toml -v true grpc.enable
+dasel put -f /cosmos/config/app.toml -v $MIN_RETAIN_BLOCKS min-retain-blocks
+dasel put -f /cosmos/config/app.toml -v $PRUNING pruning
+dasel put -f /cosmos/config/client.toml -v "tcp://localhost:${CL_RPC_PORT}" node
+
+# Word splitting is desired for the command line parameters
+# shellcheck disable=SC2086
+exec "$@" ${EXTRA_FLAGS}
