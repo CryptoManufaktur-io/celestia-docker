2656911b0ac0788f46c5c84dea7a8df64bb1820e
jianhonggalaxy <jianhong.lee@galaxy.com>
2025-04-16 09:56:57 +0000

    cleaned up bridge settings

--- celestia-node/docker-entrypoint.sh
+++ celestia-node/docker-entrypoint.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+
 __p2p_network_flag=$(echo "$NETWORK" | grep -Eo 'mocha|arabica' | sed 's/^/--p2p.network /' || echo "")
 
 if [[ ! -f /data/.initialized ]]; then
   echo "Initializing!"
-  celestia $CELESTIA_NODE_TYPE init $__p2p_network_flag --node.store /data
+  celestia $CELESTIA_NODE_TYPE init --keyring.keyname ${MONIKER} --core.ip ${CELESTIA_NODE_GRPC_IP} --core.port ${CELESTIA_NODE_GRPC_PORT} --node.store /data $__p2p_network_flag
   touch /data/.initialized
 else
   echo "Already initialized!"
 fi
-
-# Update the P2P port.
-sed -i "/^\[P2P\]/,/^$/ { /ListenAddresses\|NoAnnounceAddresses/ s/\/\(udp\|tcp\)\/[0-9]\{1,5\}/\/\1\/${P2P_PORT}/g }" /data/config.toml
 
 exec "$@" ${__p2p_network_flag} ${EXTRAS}
